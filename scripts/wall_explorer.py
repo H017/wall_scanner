8dc65f4cc002af0180c9845955ba4e8765d32bcb
Gabriel Kenderessy <g@brie.lk>
2014-08-06 16:31:24 -0400

    Snapshot angles

--- scripts/wall_explorer.py
+++ scripts/wall_explorer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import rospy
+import tf
 import actionlib
 from nav_msgs.msg import OccupancyGrid
-from move_base_msgs.msg import MoveBaseGoal
+from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
 
 import math
 from random import randint
@@ -15,9 +16,10 @@ from scipy.ndimage.filters import median_filter
 import Image, ImageDraw, ImageOps
 
 def get_path_map(data):
-    global path_map, wall_map
+    global path_map, wall_map, resolution
 
-    path_map, wall_map = raw_data_to_maps(data.data, data.info.width, data.info.height, data.info.resolution)
+    resolution = data.info.resolution
+    path_map, wall_map = raw_data_to_maps(data.data, data.info.width, data.info.height)
 
 def get_goals():
     unordered_goals = set()
@@ -29,17 +31,11 @@ def get_goals():
             y2 = y1 + chunk_height if y1 + chunk_height <= map_height else map_height
             chunk = path_map[y1:y2, x1:x2]
 
-            if np.count_nonzero(chunk) > 0: #math.floor((chunk_width * chunk_height) * processable_chunk_threshold):
+            if np.count_nonzero(chunk) > 0:
                 goal = get_goal_from_chunk(np.where(chunk != 0), x1, y1, unordered_goals)
 
                 if goal is not None:
                     unordered_goals.add(goal)
-
-    #ordered_goals = shortest_path(unordered_goals)
-    #ordered_goals = mst(unordered_goals, robot_position)
-
-
-    #pprint(ordered_goals)
 
     starting_point = (None, 99999)
 
@@ -47,35 +43,36 @@ def get_goals():
         path = shortest_path(unordered_goals.copy(), goal)
         total_distance = get_path_length(path)
 
-        print 'SP: %f' % (total_distance)
+        #print 'SP: %f' % (total_distance)
 
         if starting_point[1] > total_distance:
             starting_point = (goal, total_distance)
 
     ordered_goals = shortest_path(unordered_goals.copy(), starting_point[0])
-    set_goal_angles(ordered_goals)
+    snapshot_angles(ordered_goals)
     write_image((path_map.copy(), path_map.copy(), path_map.copy()), ordered_goals, "path_best-sp.png", True)
     write_image((wall_map.copy(), wall_map.copy(), wall_map.copy()), ordered_goals, "walls_best-sp.png")
-    #
+
+
     # ordered_goals = prim(unordered_goals.copy())
-    # set_goal_angles(ordered_goals)
+    # snapshot_angles(ordered_goals)
     # write_image((path_map.copy(), path_map.copy(), path_map.copy()), ordered_goals, "path-prim.png", True)
     #     #write_image((wall_map.copy(), wall_map.copy(), wall_map.copy()), ordered_goals, "walls-prim.png")
     # print 'Prim: %f' % (get_path_length(ordered_goals))
     #
     # ordered_goals = mst(unordered_goals.copy(), robot_position)
-    # set_goal_angles(ordered_goals)
+    # snapshot_angles(ordered_goals)
     # write_image((path_map.copy(), path_map.copy(), path_map.copy()), ordered_goals, "path-mst.png", True)
     #     #write_image((wall_map.copy(), wall_map.copy(), wall_map.copy()), ordered_goals, "walls-mst.png")
     # print 'MST: %f' % (get_path_length(ordered_goals))
     #
     # ordered_goals = shortest_path(unordered_goals.copy())
-    # set_goal_angles(ordered_goals)
+    # snapshot_angles(ordered_goals)
     # write_image((path_map.copy(), path_map.copy(), path_map.copy()), ordered_goals, "path-sp.png", True)
     #     #write_image((wall_map.copy(), wall_map.copy(), wall_map.copy()), ordered_goals, "walls-sp.png")
     # print 'SP: %f' % (get_path_length(ordered_goals))
 
-    return [tuple(map(operator.add, goal, (map_origin_y, map_origin_x, 0))) for goal in ordered_goals]
+    return [tuple(map(operator.add, goal, (map_origin_y, map_origin_x, []))) for goal in ordered_goals]
 
 def get_path_length(path):
     total_distance = 0
@@ -89,18 +86,98 @@ def get_path_length(path):
 
     return total_distance
 
+def raw_data_to_maps(raw_data, raw_width, raw_height):
+    cropped_map = np.flipud(np.reshape(np.matrix(raw_data, dtype=np.uint8), (raw_width, raw_height)))[map_origin_y:map_origin_y + map_height, map_origin_x:map_origin_x + map_width]
+    walls = cropped_map.copy()
+
+    cropped_map[cropped_map > 0] = 1
+    walls = median_filter(walls, size=(median_filter_size / resolution))
+    walls[walls == 0] = 255
+    walls[walls == 100] = 0
+
+    walls[walls == 1] = 0
+    walls[walls == 157] = 0
+    path = distance_transform_edt(1 - median_filter(cropped_map, size=median_filter_size / resolution))
+    path[path * resolution > max_wall_distance] = 0
+    path[path * resolution < min_wall_distance] = 0
+    path[path > 0] = 0x0000FF
+
+    return path, walls
+
+def get_goal_from_chunk(chunk, start_x, start_y, unordered_goals):
+    path = zip(chunk[0], chunk[1])
+    median_position = None
+
+    for pp in path:
+        total_distance = 0
+        valid = True
+        p = (pp[0] + start_y, pp[1] + start_x)
+
+        for g in unordered_goals:
+            d = distance(p, g)
+
+            if d > min_waypoint_distance:
+                total_distance += d
+            else:
+                valid = False
+                break
+
+        if valid and (median_position is None or median_position[2] < total_distance):
+            median_position = (pp[0] + start_y, pp[1] + start_x, total_distance)
+
+    return None if median_position is None else (median_position[0], median_position[1], 0)
+
+def distance(a, b):
+    return math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
+
+def heading(a, b):
+    angle = math.atan2(b[1] - a[1], b[0] - a[0])
+
+    if angle > 0:
+        orientation = angle * (360 / (2 * math.pi))
+    else:
+        orientation = (2 * math.pi + angle) * (360 / (2 * math.pi))
+
+    return orientation
+
+def snapshot_angles(goals):
+    walls = np.where(wall_map == 0)
+    walls = zip(walls[0], walls[1])
+
+    for i, goal in enumerate(goals):
+        filtered_walls = [wall for wall in walls if distance(goal, wall) < 1.1 * (max_wall_distance / resolution)]
+        angles = []
+        next_goal_orientation = heading(goals[i + 1], goal) if i + 1 < len(goals) else 0
+        previous_goal_orientation = heading(goals[i - 1], goal) if i > 0 else 0
+
+        while len(filtered_walls) > 0:
+            closest_wall = min(filtered_walls, key=lambda x:distance(goal, x))
+            filtered_walls = [wall for wall in filtered_walls if distance(closest_wall, wall) > 1.1 * (max_wall_distance / resolution)]
+            angle = heading(goal, closest_wall)
+
+            if angle != next_goal_orientation:
+                if ((360 - angle) + next_goal_orientation) % 360 >= 180:
+                    angles.append((angle - 90) % 360)
+                else:
+                    angles.append((angle + 90) % 360)
+            else:
+                if ((360 - angle) + previous_goal_orientation) % 360 >= 180:
+                    angles.append((angle + 90) % 360)
+                else:
+                    angles.append((angle - 90) % 360)
+            
+        goals[i] = (goal[0], goal[1], angles)
+
 #######################################
 #
 # Start: PRIM'S ALGORITHM
 #
 
 def prim(vertices):
-    """Given a set of cities, build a minimum spanning tree: a dict of the form {parent: [child...]},
-    where parent and children are cities, and the root of the tree is first(cities)."""
     N = len(vertices)
     start = min(vertices, key=lambda x: distance(x, robot_position))
     edges = shortest_first([(A, B) for A in vertices for B in vertices if A is not B])
-    tree = {start: []} # the first city is the root of the tree.
+    tree = {start: []}
 
     while len(tree) < N:
         (A, B) = first((A, B) for (A, B) in edges if (A in tree) and (B not in tree))
@@ -110,17 +187,13 @@ def prim(vertices):
     return preorder_uniq(tree, start, [])
 
 def shortest_first(edges):
-    "Sort a list of edges so that shortest come first."
     edges.sort(key=lambda (A, B): distance(A, B))
     return edges
 
 def first(collection):
-    "Start iterating over collection, and return the first element."
     for x in collection: return x
 
 def preorder_uniq(tree, node, result):
-    "Traverse tree in pre-order, starting at node, omitting repeated nodes."
-    # Accumulate results in the 'result' parameter, which should start with an empty list
     if node not in result:
         result.append(node)
     for child in tree[node]:
@@ -230,68 +303,22 @@ def write_image((r, g, b), goals, filename, invert=False):
         if previous_goal is not None:
             draw.line((previous_goal[1], previous_goal[0], goal[1], goal[0]), fill=0xFF000080)
 
-        rot = arrow.rotate( goal[2], expand=1 )
+        rot = arrow.rotate( goal[2][0], expand=1 )
         path.paste( rot, (goal[1] - 10, goal[0] - 10), rot )
         draw.ellipse((goal[1] - 2, goal[0] - 2, goal[1] + 2, goal[0] + 2), fill=0xFF0000FF if i > 0 else 0xFF00FF00)
         previous_goal = goal
 
     path.save(filename)
 
-def raw_data_to_maps(raw_data, raw_width, raw_height, resolution):
-    cropped_map = np.flipud(np.reshape(np.matrix(raw_data, dtype=np.uint8), (raw_width, raw_height)))[map_origin_y:map_origin_y + map_height, map_origin_x:map_origin_x + map_width]
-    walls = cropped_map.copy()
-
-    cropped_map[cropped_map > 0] = 1
-    walls = median_filter(walls, size=(median_filter_size / resolution))
-    walls[walls == 0] = 255
-    walls[walls == 100] = 0
-
-    walls[walls == 1] = 0
-    walls[walls == 157] = 0
-    path = distance_transform_edt(1 - median_filter(cropped_map, size=median_filter_size / resolution))
-    path[path * resolution > max_wall_distance] = 0
-    path[path * resolution < min_wall_distance] = 0
-    path[path > 0] = 0x0000FF
-
-    return path, walls
-
-def get_goal_from_chunk(chunk, start_x, start_y, unordered_goals):
-    path = zip(chunk[0], chunk[1])
-    median_position = None
-
-    for pp in path:
-        total_distance = 0
-        valid = True
-        p = (pp[0] + start_y, pp[1] + start_x)
-
-        for g in unordered_goals:
-            d = distance(p, g)
-
-            if d > min_waypoint_distance:
-                total_distance += d
-            else:
-                valid = False
-                break
-
-        if valid and (median_position is None or median_position[2] < total_distance):
-            median_position = (pp[0] + start_y, pp[1] + start_x, total_distance)
-
-    return None if median_position is None else (median_position[0], median_position[1], 0)
-
-def distance(a, b):
-    return math.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
-
-def set_goal_angles(goals):
-    for i, goal in enumerate(goals):
-        goals[i - 1] = (goals[i - 1][0], goals[i - 1][1], (math.atan2(goal[1] - goals[i - 1][1], goal[0] - goals[i - 1][0]) + math.pi) * 360.0 / (2.0 * math.pi))
-
 if __name__ == '__main__':
     rospy.init_node('wall_explorer')
 
+    move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     robot_position = (randint(0,500), randint(0,1200), 0) #None
+    resolution = 0
     wall_map = None
     path_map = None
-    goals = []
+    done = False
 
     # Map cropping
     map_origin_x = rospy.get_param('~map_origin_x', 500)
@@ -312,10 +339,28 @@ if __name__ == '__main__':
     min_waypoint_distance = math.sqrt(chunk_width ** 2 + chunk_height ** 2)
     #processable_chunk_threshold = rospy.get_param('~processable_chunk_threshold', 0.01)
 
+    # Snapshot parameters
+    snapshot_angle_precision = rospy.get_param('~snapshot_angle_precision', 10)
+
     rospy.Subscriber("/map", OccupancyGrid, get_path_map)
     goal_publisher = rospy.Publisher('/wall_explorer/goals', MoveBaseGoal, queue_size=100)
 
     while not rospy.is_shutdown():
-        if len(goals) == 0:
-            if robot_position is not None and path_map is not None:
-                goals = get_goals()
+        if not done and robot_position is not None and path_map is not None:
+            for i, position in enumerate(get_goals()):
+                goal = MoveBaseGoal()
+                goal.target_pose.header.frame_id = "base_link"
+                goal.target_pose.header.stamp = rospy.Time.now()
+
+                goal.target_pose.pose.position.x = position[1]
+                goal.target_pose.pose.position.y = position[0]
+
+                for angle in position[2]:
+                    goal.target_pose.pose.orientation = tf.transformations.quaternion_from_euler(0, 0, math.radians(angle))
+
+                    move_base_client.send_goal(goal)
+                    move_base_client.wait_for_result()
+                    print 'Goal: %d' % (i)
+                    rospy.sleep(3)
+
+            done = True
